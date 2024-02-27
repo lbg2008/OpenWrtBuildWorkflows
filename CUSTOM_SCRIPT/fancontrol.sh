9bc88f96110262406ed52bfadaca52bd9350521f
lbg2008 <90393989+lbg2008@users.noreply.github.com>
2024-02-27 13:13:24 +0800

    Update fancontrol.sh

--- CUSTOM_SCRIPT/fancontrol.sh
+++ CUSTOM_SCRIPT/fancontrol.sh
@@ -36,6 +36,6 @@
 #EOF
 #git apply example.patch
 # Add a feed source
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 echo 'src-git fancontrol https://github.com/JiaY-shi/fancontrol.git' >>feeds.conf.default
