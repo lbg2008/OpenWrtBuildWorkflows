fdbdcfb02d3292ec2b39427f26eac7e730ead41f
lbg2008 <90393989+lbg2008@users.noreply.github.com>
2024-02-29 01:26:15 +0800

    Update libopenssl-legacy.sh

--- CUSTOM_SCRIPT/libopenssl-legacy.sh
+++ CUSTOM_SCRIPT/libopenssl-legacy.sh
@@ -19,4 +19,6 @@
 ./scripts/feeds update packages
 sed -i 's/ +libopenssl-legacy//g' feeds/helloworld/shadowsocksr-libev/Makefile
 rm -rf feeds/packages/lang/golang
-svn co https://github.com/openwrt/packages/tree/openwrt-22.03/lang/golang feeds/packages/lang/golang
+git clone https://github.com/openwrt/packages tmptmp
+cp -rf tmptmp/lang/golang feeds/packages/lang/golang
+rm -rf tmptmp
