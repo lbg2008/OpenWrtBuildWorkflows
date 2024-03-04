ec64f92a022803d512d3c3b471143b8c2845eeab
lbg2008 <90393989+lbg2008@users.noreply.github.com>
2024-03-04 15:02:28 +0800

    Update libopenssl-legacy.sh

--- CUSTOM_SCRIPT/libopenssl-legacy.sh
+++ CUSTOM_SCRIPT/libopenssl-legacy.sh
@@ -16,6 +16,10 @@
 #patch content
 #EOF
 #git apply example.patch
-sed -i 's/ +libopenssl-legacy//g' feeds/helloworld/shadowsocksr-libev/Makefile
+#sed -i 's/ +libopenssl-legacy//g' feeds/helloworld/shadowsocksr-libev/Makefile
+#sed -i "/PKG_VERSION:=0.49.0/d" "feeds/packages/net/dnsproxy/Makefile"
+#sed -i "/PKG_HASH/d" "feeds/packages/net/dnsproxy/Makefile"
+#echo "PKG_VERSION:=0.65.2" >> "feeds/packages/net/dnsproxy/Makefile"
+#echo "PKG_HASH:=7c863404075daf76f1c6b291cc473670cbc45651af8f409e488c10a47fb73117" >> "feeds/packages/net/dnsproxy/Makefile"
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
