26a54ca8f91088edd99fc4738a95d861d1a3a724
lbg2008 <90393989+lbg2008@users.noreply.github.com>
2024-03-05 01:14:41 +0800

    Update passwall.sh

--- CUSTOM_SCRIPT/passwall.sh
+++ CUSTOM_SCRIPT/passwall.sh
@@ -1,2 +1,2 @@
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git^35a254d6184cbf957ffe9b1abef6d812273061e3' >>feeds.conf.default
