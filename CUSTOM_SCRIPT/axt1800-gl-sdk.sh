c9063e5f760a8bf587c0441c93e203bfc219e3e7
lbg2008 <90393989+lbg2008@users.noreply.github.com>
2024-03-06 07:13:23 +0800

    Update axt1800-gl-sdk.sh

--- CUSTOM_SCRIPT/axt1800-gl-sdk.sh
+++ CUSTOM_SCRIPT/axt1800-gl-sdk.sh
@@ -16,8 +16,8 @@
 #patch content
 #EOF
 #git apply example.patch
-#sed -i 's/ +libopenssl-legacy//g' feeds/helloworld/shadowsocksr-libev/Makefile
+sed -i 's/ +libopenssl-legacy//g' feeds/helloworld/shadowsocksr-libev/Makefile
 #sed -i "s/PKG_VERSION:=0.49.0/PKG_VERSION:=0.65.2/g" "feeds/packages/net/dnsproxy/Makefile"
 #sed -i "s/PKG_HASH:=4a6a698d830195fd8c04dd32c67e872bfd304fc39dbdaa982935892566b3ae37/PKG_HASH:=7c863404075daf76f1c6b291cc473670cbc45651af8f409e488c10a47fb73117/g" "feeds/packages/net/dnsproxy/Makefile"
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
