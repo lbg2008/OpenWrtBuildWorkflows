1364a4c741fdf7fc480228008a333f31804b236f
lbg2008 <90393989+lbg2008@users.noreply.github.com>
2024-03-07 19:48:41 +0800

    Update GL-AXT1800-Telecominfraproject.sh

--- CUSTOM_SCRIPT/GL-AXT1800-Telecominfraproject.sh
+++ CUSTOM_SCRIPT/GL-AXT1800-Telecominfraproject.sh
@@ -1,1 +1,44 @@
-1
+#!/bin/bash
+# Copyright (c) 2022-2023 Curious <https://www.curious.host>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+# 
+# https://github.com/Curious-r/OpenWrtBuildWorkflows
+# Description: Automatically check OpenWrt source code update and build it. No additional keys are required.
+#-------------------------------------------------------------------------------------------------------
+#
+#
+# This script will run before feeds update, something you want to do at that moment should be written here.
+# A common function of this script is to modify the cloned OpenWrt source code. 
+#
+# For instance, you can edit the feeds.conf.default to induct packages you need.
+# This is followed by some editing examples.
+# # Clear the feeds.conf.default and append the feed sources you need one by one:
+#cat /dev/null > a.txt
+#echo 'src-git-full packages https://git.openwrt.org/feed/packages.git;openwrt-22.03' >> feeds.conf.default
+#echo 'src-git-full luci https://git.openwrt.org/project/luci.git;openwrt-22.03' >> feeds.conf.default
+#echo 'src-git-full routing https://git.openwrt.org/feed/routing.git;openwrt-22.03' >> feeds.conf.default
+#echo 'src-git-full telephony https://git.openwrt.org/feed/telephony.git;openwrt-22.03' >> feeds.conf.default
+# # Replace a feed source with what you want:
+#sed '/feeds-name/'d feeds.conf.default
+#echo 'method feed-name path/URL' >> feeds.conf.default
+# # Uncomment a feed source:
+#sed -i 's/^#\(.*feed-name\)/\1/' feeds.conf.default
+# # Replace src-git-full with src-git to reduce the depth of cloning:
+#sed -i 's/src-git-full/src-git/g' feeds.conf.default
+#
+# You can also modify the source code by patching.
+# # Here's a template for patching:
+#touch example.patch
+#cat>example.patch<<EOF
+#patch content
+#EOF
+#git apply example.patch
+# Add a feed source
+#echo 'src-git nss_packages https://github.com/qosmio/nss-packages.git;NSS-12.4-K6.1' >>feeds.conf.default
+echo 'src-git fancontrol https://github.com/lbg2008/fancontrol.git' >>feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
+#echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
