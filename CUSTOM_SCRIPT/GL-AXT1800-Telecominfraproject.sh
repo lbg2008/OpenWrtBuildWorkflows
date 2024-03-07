ed5db2af82777718fc0fd301908bc2ff44733331
lbg2008 <90393989+lbg2008@users.noreply.github.com>
2024-03-07 22:11:17 +0800

    Update GL-AXT1800-Telecominfraproject.sh

--- CUSTOM_SCRIPT/GL-AXT1800-Telecominfraproject.sh
+++ CUSTOM_SCRIPT/GL-AXT1800-Telecominfraproject.sh
@@ -40,5 +40,6 @@
 echo 'src-git fancontrol https://github.com/lbg2008/fancontrol.git' >>feeds.conf.default
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
+./scripts/feeds update -a &&./scripts/feeds install -a
 #echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
