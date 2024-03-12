4de726bd651ab4305d9658679be17699c14070f6
lbg2008 <90393989+lbg2008@users.noreply.github.com>
2024-03-12 12:53:07 +0800

    Update libopenssl-legacy.sh

--- CUSTOM_SCRIPT/libopenssl-legacy.sh
+++ CUSTOM_SCRIPT/libopenssl-legacy.sh
@@ -22,3 +22,6 @@ sed -i "s/PKG_VERSION:=0.49.0/PKG_VERSION:=0.65.2/g" "feeds/packages/net/dnsprox
 sed -i "s/PKG_HASH:=4a6a698d830195fd8c04dd32c67e872bfd304fc39dbdaa982935892566b3ae37/PKG_HASH:=7c863404075daf76f1c6b291cc473670cbc45651af8f409e488c10a47fb73117/g" "feeds/packages/net/dnsproxy/Makefile"
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+rm -rf feeds/gl_feed_common/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/gl_feed_common/golang
+
