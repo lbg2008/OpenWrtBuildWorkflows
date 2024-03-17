306af58d128de87875320c8c34142891169a69ce
lbg2008 <90393989+lbg2008@users.noreply.github.com>
2024-03-17 22:46:04 +0800

    Update GL-AXT1800-Telecominfraproject2.sh

--- CUSTOM_SCRIPT/GL-AXT1800-Telecominfraproject2.sh
+++ CUSTOM_SCRIPT/GL-AXT1800-Telecominfraproject2.sh
@@ -1,1 +1,22 @@
-GL-AXT1800-Telecominfraproject.sh
+#!/bin/bash
+# Copyright (c) 2022-2023 Curious <https://www.curious.host>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+# 
+# https://github.com/Curious-r/OpenWrtBuildWorkflows
+# Description: Automatically check OpenWrt source code update and build it. No additional keys are required.
+#-------------------------------------------------------------------------------------------------------
+#
+#
+# Patching is generally recommended.
+# # Here's a template for patching:
+#touch example.patch
+#cat>example.patch<<EOF
+#patch content
+#EOF
+#git apply example.patch
+#sed -i "s/+chinadns-ng +dns2socks/+dns2socks/g" "feeds/passwall/luci-app-passwall/Makefile"
+sed -i 's/ +libopenssl-legacy//g' feeds/helloworld/shadowsocksr-libev/Makefile
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
