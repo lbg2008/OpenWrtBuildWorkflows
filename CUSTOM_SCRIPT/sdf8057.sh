9dd15179e1d4656d0fa65f810541d83f154d1468
lbg2008 <90393989+lbg2008@users.noreply.github.com>
2024-03-19 06:51:36 +0800

    Update sdf8057.sh

--- CUSTOM_SCRIPT/sdf8057.sh
+++ CUSTOM_SCRIPT/sdf8057.sh
@@ -1,5 +1,6 @@
 rm -rf feeds/packages/net/dnsproxy
-curl -L https://alist-home.320888.xyz:2096/d/Oracle/App/dnsproxy-0.62.zip -o feeds/packages/net/dnsproxy.zip
+#curl -L https://alist-home.320888.xyz:2096/d/Oracle/App/dnsproxy-0.62.zip -o feeds/packages/net/dnsproxy.zip
+curl -L https://alist.320888.xyz/d/Oracle-Chuncheon/APP/dnsproxy-0.62.zip -o feeds/packages/net/dnsproxy.zip
 unzip -d feeds/packages/net/dnsproxy feeds/packages/net/dnsproxy.zip
 rm feeds/packages/net/dnsproxy.zip
 rm -rf feeds/packages/lang/golang
