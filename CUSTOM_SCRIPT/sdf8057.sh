f8ad45345e47e135c6513094a1a0adf4be393813
lbg2008 <90393989+lbg2008@users.noreply.github.com>
2024-03-19 06:36:03 +0800

    Update sdf8057.sh

--- CUSTOM_SCRIPT/sdf8057.sh
+++ CUSTOM_SCRIPT/sdf8057.sh
@@ -1,2 +1,4 @@
-sed -i "s/PKG_VERSION:=0.49.0/PKG_VERSION:=0.65.2/g" "feeds/packages/net/dnsproxy/Makefile"
-sed -i "s/PKG_HASH:=4a6a698d830195fd8c04dd32c67e872bfd304fc39dbdaa982935892566b3ae37/PKG_HASH:=7c863404075daf76f1c6b291cc473670cbc45651af8f409e488c10a47fb73117/g" "feeds/packages/net/dnsproxy/Makefile"
+rm -rf feeds/packages/net/dnsproxy
+curl -L https://alist-home.320888.xyz:2096/d/Oracle/App/dnsproxy.zip -o feeds/packages/net/dnsproxy.zip
+unzip -d feeds/packages/net/dnsproxy feeds/packages/net/dnsproxy.zip
+rm feeds/packages/net/dnsproxy.zip
