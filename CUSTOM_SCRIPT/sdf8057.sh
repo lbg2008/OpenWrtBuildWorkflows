98cbc9ad845a1a666001eb273932a3f2594487fc
lbg2008 <90393989+lbg2008@users.noreply.github.com>
2024-03-19 06:07:54 +0800

    Update and rename sdf to sdf8057.sh

---
+++ CUSTOM_SCRIPT/sdf8057.sh
@@ -0,0 +1,2 @@
+sed -i "s/PKG_VERSION:=0.49.0/PKG_VERSION:=0.52.0/g" "feeds/packages/net/dnsproxy/Makefile"
+sed -i "s/PKG_HASH:=4a6a698d830195fd8c04dd32c67e872bfd304fc39dbdaa982935892566b3ae37/PKG_HASH:=e650ab9df52e33de1431690b84e700e88212dfc14684d0a88dd148f3cfc8336a/g" "feeds/packages/net/dnsproxy/Makefile"
