777afae136d4485922e08a70e8acdfb063b2038f
lbg2008 <90393989+lbg2008@users.noreply.github.com>
2024-03-19 06:48:01 +0800

    Update sdf8057.sh

--- CUSTOM_SCRIPT/sdf8057.sh
+++ CUSTOM_SCRIPT/sdf8057.sh
@@ -2,3 +2,5 @@ rm -rf feeds/packages/net/dnsproxy
 curl -L https://alist-home.320888.xyz:2096/d/Oracle/App/dnsproxy-0.62.zip -o feeds/packages/net/dnsproxy.zip
 unzip -d feeds/packages/net/dnsproxy feeds/packages/net/dnsproxy.zip
 rm feeds/packages/net/dnsproxy.zip
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
