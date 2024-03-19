82870f4189b4d398935c4b844a9bb354cb2b91af
lbg2008 <90393989+lbg2008@users.noreply.github.com>
2024-03-19 23:27:02 +0800

    Update GL-AXT1800-SDK-SSR.sh

--- CUSTOM_SCRIPT/GL-AXT1800-SDK-SSR.sh
+++ CUSTOM_SCRIPT/GL-AXT1800-SDK-SSR.sh
@@ -5,5 +5,7 @@ unzip -d feeds/packages/net/dnsproxy feeds/glinet/dnsproxy.zip
 rm feeds/glinet/dnsproxy.zip
 ./scripts/feeds update -a &&./scripts/feeds install -a
 sed -i 's/ +libopenssl-legacy//g' feeds/helloworld/shadowsocksr-libev/Makefile
+rm -rf feeds/gl_feed_common/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/gl_feed_common/golang
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
