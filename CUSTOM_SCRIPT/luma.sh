123d42507e2f4279e980f101f012be0eda8b5bc2
lbg2008 <90393989+lbg2008@users.noreply.github.com>
2024-03-20 04:50:41 +0800

    Update luma.sh

--- CUSTOM_SCRIPT/luma.sh
+++ CUSTOM_SCRIPT/luma.sh
@@ -5,7 +5,7 @@ rm -rf feeds/packages/net/xray-core
 curl -L https://alist.320888.xyz/d/Oracle-Chuncheon/APP/dnsproxy-0.62.zip -o feeds/packages/net/dnsproxy.zip
 unzip -d feeds/packages/net/dnsproxy feeds/packages/net/dnsproxy.zip
 rm feeds/packages/net/dnsproxy.zip
-./scripts/feeds update -a &&./scripts/feeds install -a
+./scripts/feeds install -a
 sed -i 's/ +libopenssl-legacy//g' feeds/helloworld/shadowsocksr-libev/Makefile
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
