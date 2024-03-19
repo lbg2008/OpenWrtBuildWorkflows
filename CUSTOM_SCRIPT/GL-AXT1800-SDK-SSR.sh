88d161ed973690ea83844f64276919bce60fe0cb
lbg2008 <90393989+lbg2008@users.noreply.github.com>
2024-03-20 04:07:43 +0800

    Update GL-AXT1800-SDK-SSR.sh

--- CUSTOM_SCRIPT/GL-AXT1800-SDK-SSR.sh
+++ CUSTOM_SCRIPT/GL-AXT1800-SDK-SSR.sh
@@ -3,7 +3,7 @@ rm -rf feeds/packages/net/xray-core
 curl -L https://alist.320888.xyz/d/Oracle-Chuncheon/APP/dnsproxy-0.62.zip -o feeds/glinet/dnsproxy.zip
 unzip -d feeds/packages/net/dnsproxy feeds/glinet/dnsproxy.zip
 rm feeds/glinet/dnsproxy.zip
-./scripts/feeds update -a &&./scripts/feeds install -a
+./scripts/feeds install -a
 sed -i 's/ +libopenssl-legacy//g' feeds/helloworld/shadowsocksr-libev/Makefile
 rm -rf feeds/gl_feed_common/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/gl_feed_common/golang
