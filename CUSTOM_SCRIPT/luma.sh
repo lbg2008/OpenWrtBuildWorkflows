0171689530f6d8428d6fa7c68a062ae1056ba904
lbg2008 <90393989+lbg2008@users.noreply.github.com>
2024-03-19 17:34:44 +0800

    Update luma.sh

--- CUSTOM_SCRIPT/luma.sh
+++ CUSTOM_SCRIPT/luma.sh
@@ -1,10 +1,10 @@
 rm -rf feeds/packages/net/dnsproxy
 #curl -L https://alist-home.320888.xyz:2096/d/Oracle/App/dnsproxy-0.62.zip -o feeds/packages/net/dnsproxy.zip
-#curl -L https://alist.320888.xyz/d/Oracle-Chuncheon/APP/dnsproxy-0.52.0.zip -o feeds/packages/net/dnsproxy.zip
-curl -L https://alist.320888.xyz/d/Oracle-Chuncheon/APP/dnsproxy-0.62.zip -o feeds/packages/net/dnsproxy.zip
+curl -L https://alist.320888.xyz/d/Oracle-Chuncheon/APP/dnsproxy-0.52.0.zip -o feeds/packages/net/dnsproxy.zip
+#curl -L https://alist.320888.xyz/d/Oracle-Chuncheon/APP/dnsproxy-0.62.zip -o feeds/packages/net/dnsproxy.zip
 unzip -d feeds/packages/net/dnsproxy feeds/packages/net/dnsproxy.zip
 ./scripts/feeds update -a &&./scripts/feeds install -a
 rm feeds/packages/net/dnsproxy.zip
 sed -i 's/ +libopenssl-legacy//g' feeds/helloworld/shadowsocksr-libev/Makefile
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
