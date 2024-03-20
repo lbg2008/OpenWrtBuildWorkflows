805e028aa21c4b1baf2519e5ed23e0cd7606a7da
lbg2008 <90393989+lbg2008@users.noreply.github.com>
2024-03-20 08:42:29 +0800

    Update luma-1.sh

--- CUSTOM_SCRIPT/luma-1.sh
+++ CUSTOM_SCRIPT/luma-1.sh
@@ -1,1 +1,3 @@
+sed -i "/helloworld/d" "feeds.conf.default"
+echo "src-git helloworld https://github.com/lbg2008/helloworld.git;main" >> "feeds.conf.default"
 echo 'src-git fancontrol https://github.com/lbg2008/fancontrol.git' >>feeds.conf.default
