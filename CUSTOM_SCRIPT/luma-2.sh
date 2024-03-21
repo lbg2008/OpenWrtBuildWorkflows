11ef94fe86602508df0663eab68010c91627ea85
lbg2008 <90393989+lbg2008@users.noreply.github.com>
2024-03-21 23:15:47 +0800

    Update luma-2.sh

--- CUSTOM_SCRIPT/luma-2.sh
+++ CUSTOM_SCRIPT/luma-2.sh
@@ -9,8 +9,7 @@ sed -i 's/ +libopenssl-legacy//g' feeds/helloworld/shadowsocksr-libev/Makefile
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/lbg2008/packages_lang_golang -b 22.x feeds/packages/lang/golang
 rm -rf feeds/fancontrol/dnsproxy
-#curl -L https://alist-home.320888.xyz:2096/d/Oracle/App/dnsproxy-0.62.zip -o feeds/fancontrol/dnsproxy.zip
 #curl -L https://alist.320888.xyz/d/Oracle-Chuncheon/APP/dnsproxy-0.52.0.zip -o feeds/fancontrol/dnsproxy.zip
-curl -L https://alist.320888.xyz/d/Oracle-Chuncheon/APP/dnsproxy-0.62.zip -o feeds/fancontrol/dnsproxy.zip
+curl -L https://alist.320888.xyz/d/Oracle-Chuncheon/APP/dnsproxy.zip -o feeds/fancontrol/dnsproxy.zip
 unzip -d feeds/fancontrol/dnsproxy feeds/fancontrol/dnsproxy.zip
 rm feeds/fancontrol/dnsproxy.zip
