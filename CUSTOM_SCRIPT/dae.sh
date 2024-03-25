d6d0577b3a3f7b577f8fccd5c4d4e59128be5da6
lbg2008 <90393989+lbg2008@users.noreply.github.com>
2024-03-25 18:00:23 +0800

    Create dae.sh

---
+++ CUSTOM_SCRIPT/dae.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/sbwml/luci-app-dae package/dae
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+sudo apt-get update
+sudo apt-get install -y clang-13
