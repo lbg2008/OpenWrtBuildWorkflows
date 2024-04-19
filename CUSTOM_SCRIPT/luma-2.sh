2efcb2c90da910dcf6d08d1a3df99ef910152ded
lbg2008 <90393989+lbg2008@users.noreply.github.com>
2024-04-19 14:55:43 +0800

    Update luma-2.sh

--- CUSTOM_SCRIPT/luma-2.sh
+++ CUSTOM_SCRIPT/luma-2.sh
@@ -13,5 +13,5 @@ rm -rf feeds/fancontrol/dnsproxy
 curl -L https://alist.320888.xyz/d/Oracle-Chuncheon/APP/dnsproxy.zip -o feeds/fancontrol/dnsproxy.zip
 unzip -d feeds/fancontrol/dnsproxy feeds/fancontrol/dnsproxy.zip
 rm feeds/fancontrol/dnsproxy.zip
-sed -i "s/PKG_VERSION:=0.66.0/PKG_VERSION:=0.69.2/g" "feeds/fancontrol/dnsproxy/Makefile"
-sed -i "s/PKG_HASH:=6928b109fb1080fec2aadc0cad20d0c08d13b5ff5db1a7c82ecfe200eec21326/PKG_HASH:=aa1cea0eea683bde017acbb30c09c96b24b30133e157e743666be900ad7560ea/g" "feeds/fancontrol/dnsproxy/Makefile"
+#sed -i "s/PKG_VERSION:=0.66.0/PKG_VERSION:=0.69.2/g" "feeds/fancontrol/dnsproxy/Makefile"
+#sed -i "s/PKG_HASH:=6928b109fb1080fec2aadc0cad20d0c08d13b5ff5db1a7c82ecfe200eec21326/PKG_HASH:=aa1cea0eea683bde017acbb30c09c96b24b30133e157e743666be900ad7560ea/g" "feeds/fancontrol/dnsproxy/Makefile"
