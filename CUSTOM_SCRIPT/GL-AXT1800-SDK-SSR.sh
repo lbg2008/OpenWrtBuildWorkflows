d06562780cc598cba2ba83530bc48459b94c9245
lbg2008 <90393989+lbg2008@users.noreply.github.com>
2024-04-19 15:37:28 +0800

    Update GL-AXT1800-SDK-SSR.sh

--- CUSTOM_SCRIPT/GL-AXT1800-SDK-SSR.sh
+++ CUSTOM_SCRIPT/GL-AXT1800-SDK-SSR.sh
@@ -6,8 +6,6 @@ git clone https://github.com/lbg2008/packages_lang_golang -b 22.x feeds/gl_feed_
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/lbg2008/packages_lang_golang -b 22.x feeds/packages/lang/golang
 rm -rf feeds/glinet/dnsproxy
-curl -L https://alist.320888.xyz/d/Oracle-Chuncheon/APP/dnsproxy.zip -o feeds/glinet/dnsproxy.zip
+curl -L https://alist-home.320888.xyz/Oracle/App/dnsproxy.zip -o feeds/glinet/dnsproxy.zip
 unzip -d feeds/glinet/dnsproxy feeds/glinet/dnsproxy.zip
 rm feeds/glinet/dnsproxy.zip
-#sed -i "s/PKG_VERSION:=0.66.0/PKG_VERSION:=0.69.2/g" "feeds/glinet/dnsproxy/Makefile"
-#sed -i "s/PKG_HASH:=6928b109fb1080fec2aadc0cad20d0c08d13b5ff5db1a7c82ecfe200eec21326/PKG_HASH:=aa1cea0eea683bde017acbb30c09c96b24b30133e157e743666be900ad7560ea/g" "feeds/glinet/dnsproxy/Makefile"
