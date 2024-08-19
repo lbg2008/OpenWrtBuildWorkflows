9f3a20c30a8bbc59ba697990f61c9b985b8a1462
lbg2008 <90393989+lbg2008@users.noreply.github.com>
2024-08-19 13:04:42 +0800

    Update luma-1.sh

--- CUSTOM_SCRIPT/luma-1.sh
+++ CUSTOM_SCRIPT/luma-1.sh
@@ -1,3 +1,5 @@
 sed -i "/helloworld/d" "feeds.conf.default"
 echo "src-git helloworld https://github.com/lbg2008/helloworld.git;main" >> "feeds.conf.default"
 echo 'src-git fancontrol https://github.com/lbg2008/fancontrol.git' >>feeds.conf.default
+mkdir package/openwrt-packages
+git clone https://github.com/OrayOS/OpenOray package/oray-packages
