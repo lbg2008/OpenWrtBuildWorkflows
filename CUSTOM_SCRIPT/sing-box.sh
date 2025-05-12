7f4f22f75f9b3fc240ae8ffd24d2d6b9522b882e
lbg2008 <90393989+lbg2008@users.noreply.github.com>
2025-05-12 14:35:25 +0800

    Create sing-box.sh

---
+++ CUSTOM_SCRIPT/sing-box.sh
@@ -0,0 +1,5 @@
+rm -rf package/network/services/sing-box
+mkdir -p package/network/services/sing-box
+git clone https://github.com/SagerNet/sing-box.git package/network/services/sing-box
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
